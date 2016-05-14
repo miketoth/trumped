7a738dc96add31aa3f64c4dc3d0d64ab8fd294e5
Michael Toth <tofudrummer@gmail.com>
2016-05-14 16:43:39 -0700

    Addes Trump TM 50% of the time on every page

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,25 +1,42 @@
-﻿console.log('Hello, world! This is the Chrome Extension Template speaking.');
+﻿var all = document.body.getElementsByTagName('*');
+var terms;
+var statement;
+var newText = [];
+var words;
+var shouldInsert;
 
-// ============================================================
-// STORAGE
-// ------------------------------------------------------------
-// https://developer.chrome.com/extensions/storage
-// ============================================================
-chrome.storage.local.get('count', function(data) {
-    var count = 1;
-    if (data.count) {
-        count = data.count + 1;
-    }
-    console.log('You have loaded ' + count + ' web page(s) since you have installed the Chrome Extension Template.');
-    chrome.storage.local.set({ 'count': count });
-});
+var ignore = {
+        "STYLE": 0,
+        "SCRIPT": 0,
+        "NOSCRIPT": 0,
+        "IFRAME": 0,
+        "OBJECT": 0,
+        "INPUT": 0,
+        "FORM": 0,
+        "TEXTAREA": 0
+};
 
-// ============================================================
-// MESSAGE PASSING
-// ------------------------------------------------------------
-// https://developer.chrome.com/extensions/messaging
-// ============================================================
-chrome.runtime.sendMessage({ 'message': 'hello' }, function(response) {
-    // To see how this message was handled, see js/background.js.
-    console.log('The response from your message was: ' + response.message);
-});
+for(i=0;i<all.length;i++) {
+  if(all[i].tagName in ignore) {
+  } else if(all[i].text && all[i].text.length > 0) {
+    words = all[i].text.split(" ");
+
+    words.forEach(function(word) {
+      if(word.match(/[a-z]/i)) {
+        statement = nlp_compromise.statement(word);
+        shouldInsert = Math.random();
+
+        // split text into words and then do the nlp
+        if(statement && statement.terms && statement.terms[0] && statement.terms[0].tag === "Noun" && shouldInsert < 0.5) {
+          terms = statement.terms[0];
+          newText.push("Trump " + terms.text + " ™");
+        } else {
+          newText.push(word);
+        }
+      } else { newText.push(word); }
+    });
+
+    all[i].text = newText.join(" ");
+    newText = [];
+  }
+};
